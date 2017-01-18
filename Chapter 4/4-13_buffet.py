e1af50640834fcc9d8949d7a23b91e70015d450e
Matthew Compton <matthewc174@gmail.com>
2017-01-17 22:55:18 -0500

    Finishing chapter 4

---
+++ Chapter 4/4-13_buffet.py
@@ -0,0 +1,8 @@
+my_foods = ('pizza','falafel','carrot cake','oranges','apples')
+for food in my_foods:
+    print(food.title())
+
+
+my_foods = ('fried rice','falafel','salmon','oranges','apples')
+for food in my_foods:
+    print(food.title())
