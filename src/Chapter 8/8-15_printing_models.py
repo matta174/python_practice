d5b862c020a5ffcebca641435aa7f51ac79ec447
Matthew Compton <matthewc174@gmail.com>
2017-02-08 20:12:25 -0500

    Finished 8

---
+++ src/Chapter 8/8-15_printing_models.py
@@ -0,0 +1,7 @@
+import printing_functions as pf
+
+unprinted_designs = ['iphone','cat','car']
+completed_models = []
+
+pf.print_models(unprinted_designs,completed_models)
+pf.show_completed_models(completed_models)
---
+++ src/Chapter 8/8-16_imports.py
@@ -0,0 +1,4 @@
+import testfor8_16
+from testfor8_16 import build_profile as bp
+
+me=bp('Matthew','Compton',location = 'Dahlonega', field= 'Computer Science',learning_python = True)
